24b1a75222770da90e87a8e07ecb2a3c778788b5
Saitama <roynes2012@gmail.com>
2018-07-13 14:58:40 +0800

    Updates
    - Updated composer.json
    - Removed old subdomain folder

--- subdomains/company-group/routes/web.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-Route::domain('company-group.sample.test')->group(function() {
-    Route::get('/', function() {
-        return view('company-group.views.welcome');
-    })->name('company.group.index');
-});
--- subdomains/company-one/routes/web.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-Route::domain('company-one.sample.test')->group(function() {
-    Route::get('/', function() {
-        return view('company-one.views.welcome');
-    });
-});
